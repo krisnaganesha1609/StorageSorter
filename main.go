684913e2fd59c77187a02f82964e056d39471726
riarumoda <justinebudiwijaya@gmail.com>
2024-05-15 01:30:08 +0800

    main: a little update

--- main.go
+++ main.go
@@ -62,74 +62,20 @@ func main() {
 // ----> Prosedur Untuk Menu Utama <----
 func konfigurasiDataProduk(data *Data, n *int) {
 	var det int
-	fmt.Println("Sub-Menu")
-	fmt.Println("1. Tambah Data")
-	fmt.Println("2. Tampilkan Semua Data")
-	fmt.Println("3. Kembali ke Menu Utama")
+	menuHeaderKonfigurasiDataProduk()
+	menuOptionsKonfigurasiDataProduk()
 	fmt.Println("Masukkan menu: ")
 	fmt.Print(">>>> ")
 	fmt.Scan(&det)
 	for det != 3 {
 		if det == 1 && *n < MAXPRODUCT {
-			fmt.Println("---------------------------")
-			fmt.Println("P E N G I S I A N   D A T A")
-			fmt.Println("---------------------------")
-			fmt.Println("Nama Produk:")
-			fmt.Print(">>>> ")
-			fmt.Scan(&data[*n].namaProduk)
-			for data[*n].namaProduk == "" {
-				fmt.Println("Nama produk kosong! Mohon masukkan nama produk: ")
-				fmt.Print(">>>> ")
-				fmt.Scan(&data[*n].namaProduk)
-			}
-			fmt.Println("---------------------------")
-			fmt.Println("Merek Produk:")
-			fmt.Print(">>>>")
-			fmt.Scan(&data[*n].merek)
-			for data[*n].merek == "" {
-				fmt.Println("Merek produk kosong! Mohon masukkan merek produk: ")
-				fmt.Print(">>>> ")
-				fmt.Scan(&data[*n].merek)
-			}
-			fmt.Println("---------------------------")
-			fmt.Println("Jenis Produk:")
-			fmt.Print(">>>> ")
-			fmt.Scan(&data[*n].jenis)
-			for data[*n].jenis == "" {
-				fmt.Println("Jenis produk kosong! Mohon masukkan jenis produk: ")
-				fmt.Print(">>>> ")
-				fmt.Scan(&data[*n].jenis)
-			}
-			fmt.Println("---------------------------")
-			fmt.Println("Harga Produk:")
-			fmt.Print(">>>> Rp.")
-			fmt.Scan(&data[*n].harga)
-			for data[*n].harga == 0 {
-				fmt.Println("Harga produk kosong! Mohon masukkan harga produk: ")
-				fmt.Print(">>>> Rp.")
-				fmt.Scan(&data[*n].harga)
-			}
-			fmt.Println("---------------------------")
-			fmt.Println("Stok Produk:")
-			fmt.Print(">>>> ")
-			fmt.Scan(&data[*n].stok)
-			for data[*n].stok == -1 {
-				fmt.Println("Stok produk kosong! Mohon masukkan stok produk: ")
-				fmt.Print(">>>> ")
-				fmt.Scan(&data[*n].stok)
-			}
-			fmt.Println("---------------------------")
-			fmt.Println("Data Ke-", *n+1, "Berhasil Diinput")
-			*n++
+			inputDataProduk(data, n)
 		} else if det == 1 && *n >= MAXPRODUCT {
 			fmt.Println("Memori telah habis!")
 		} else if det == 2 {
 			tampilSemuaDataProduk(*data, *n)
 		}
-		fmt.Println("Sub-Menu")
-		fmt.Println("1. Tambah Data")
-		fmt.Println("2. Tampilkan Semua Data")
-		fmt.Println("3. Kembali ke Menu Utama")
+		menuOptionsKonfigurasiDataProduk()
 		fmt.Println("Masukkan menu: ")
 		fmt.Print(">>>> ")
 		fmt.Scan(&det)
@@ -142,12 +88,83 @@ func pencatatanTransaksi() {
 
 }
 
+// Input data produk
+func inputDataProduk(data *Data, n *int) {
+	fmt.Println("---------------------------")
+	fmt.Println("P E N G I S I A N   D A T A")
+	fmt.Println("---------------------------")
+	fmt.Println("Nama Produk:")
+	fmt.Print(">>>> ")
+	fmt.Scan(&data[*n].namaProduk)
+	for data[*n].namaProduk == "" {
+		fmt.Println("Nama produk kosong! Mohon masukkan nama produk: ")
+		fmt.Print(">>>> ")
+		fmt.Scan(&data[*n].namaProduk)
+	}
+	fmt.Println("---------------------------")
+	fmt.Println("Merek Produk:")
+	fmt.Print(">>>>")
+	fmt.Scan(&data[*n].merek)
+	for data[*n].merek == "" {
+		fmt.Println("Merek produk kosong! Mohon masukkan merek produk: ")
+		fmt.Print(">>>> ")
+		fmt.Scan(&data[*n].merek)
+	}
+	fmt.Println("---------------------------")
+	fmt.Println("Jenis Produk:")
+	fmt.Print(">>>> ")
+	fmt.Scan(&data[*n].jenis)
+	for data[*n].jenis == "" {
+		fmt.Println("Jenis produk kosong! Mohon masukkan jenis produk: ")
+		fmt.Print(">>>> ")
+		fmt.Scan(&data[*n].jenis)
+	}
+	fmt.Println("---------------------------")
+	fmt.Println("Harga Produk:")
+	fmt.Print(">>>> Rp.")
+	fmt.Scan(&data[*n].harga)
+	for data[*n].harga == 0 {
+		fmt.Println("Harga produk kosong! Mohon masukkan harga produk: ")
+		fmt.Print(">>>> Rp.")
+		fmt.Scan(&data[*n].harga)
+	}
+	fmt.Println("---------------------------")
+	fmt.Println("Stok Produk:")
+	fmt.Print(">>>> ")
+	fmt.Scan(&data[*n].stok)
+	for data[*n].stok == -1 {
+		fmt.Println("Stok produk kosong! Mohon masukkan stok produk: ")
+		fmt.Print(">>>> ")
+		fmt.Scan(&data[*n].stok)
+	}
+	fmt.Println("---------------------------")
+	fmt.Println("Data Ke-", *n+1, "Berhasil Diinput")
+	*n++
+}
+
 //TODO: Buat Prosedur dan Function untuk Sub-Menu dari Konfigurasi Data Produk
 
 func tampilSemuaDataProduk(data Data, n int) {
 	var det int
+	menuHeaderKonfigurasiDataProduk()
+	for det != 6 {
+		menuOptionsTampilSemuaProduk()
+		fmt.Println("Masukkan menu: ")
+		fmt.Print(">>>> ")
+		if det == 1 {
+			showTampilSemuaProduk(data, n)
+		} else if det == 2 {
+			editTampilSemuaProduk(&data)
+		}
+	}
+}
+
+// Tampilkan data
+// TODO: need a better placement for the interface header
+// TODO_2: is calling tampilSemuaDataProduk() is necessary?
+func showTampilSemuaProduk(data Data, n int) {
 	fmt.Println("|------------------------------------------------------------------------------|")
-	fmt.Println("|                            T A B E L   D A T A                               |")
+	fmt.Println("|                      T A B E L - D A T A - P R O D U K                       |")
 	fmt.Println("|------------------------------------------------------------------------------|")
 	fmt.Println("| No. | Nama Produk | Merek Produk | Jenis Produk | Harga Produk | Stok Produk |")
 	for i := 0; i < n; i++ {
@@ -155,14 +172,75 @@ func tampilSemuaDataProduk(data Data, n int) {
 	}
 	fmt.Println("|------------------------------------------------------------------------------|")
 	fmt.Println()
-	for det != 5 {
-		fmt.Println("  D A T A   P R O D U K  ")
-		fmt.Println("1. Edit Data")
-		fmt.Println("2. Hapus Data")
-		fmt.Println("3. Cari Data")
-		fmt.Println("4. Urutkan Data")
-		fmt.Println("Masukkan menu: ")
+	tampilSemuaDataProduk(data, n)
+}
+
+// Edit Data
+/*
+ * TODO: asumsi user mengira index 0 dari data adalah Data pertama
+ * atau 1, akan akan aneh jika user mengedit data 1 dan yang teredit
+ * menjadi data kedua.
+ * TODO_2: asumsi user adalah tinkerer dan mencoba untuk input 0 untuk
+ * select kolom data, kita buat user untuk input ulang dengan benar.
+ * extra untuk mengecek apakah user meng-select kolom data lebih dari
+ * MAXPRODUCT.
+ * TODO_3: do we need to use by-pointers for the variables?
+ */
+func editTampilSemuaProduk(data *Data) {
+	var n int
+	fmt.Print("Masukkan kolom data yang akan diedit: ")
+	fmt.Scan(&n)
+	if n == 0 && n > MAXPRODUCT {
+		fmt.Println("Masukkan kolom data yang benar!")
+		fmt.Print("Masukkan kolom data yang akan diedit: ")
+		fmt.Scan(&n)
+	} else {
+		fmt.Println("Nama Produk:")
 		fmt.Print(">>>> ")
+		fmt.Scan(&data[n-1].namaProduk)
+		for data[n-1].namaProduk == "" {
+			fmt.Println("Nama produk kosong! Mohon masukkan nama produk: ")
+			fmt.Print(">>>> ")
+			fmt.Scan(&data[n-1].namaProduk)
+		}
+		fmt.Println("---------------------------")
+		fmt.Println("Merek Produk:")
+		fmt.Print(">>>>")
+		fmt.Scan(&data[n-1].merek)
+		for data[n-1].merek == "" {
+			fmt.Println("Merek produk kosong! Mohon masukkan merek produk: ")
+			fmt.Print(">>>> ")
+			fmt.Scan(&data[n-1].merek)
+		}
+		fmt.Println("---------------------------")
+		fmt.Println("Jenis Produk:")
+		fmt.Print(">>>> ")
+		fmt.Scan(&data[n-1].jenis)
+		for data[n-1].jenis == "" {
+			fmt.Println("Jenis produk kosong! Mohon masukkan jenis produk: ")
+			fmt.Print(">>>> ")
+			fmt.Scan(&data[n-1].jenis)
+		}
+		fmt.Println("---------------------------")
+		fmt.Println("Harga Produk:")
+		fmt.Print(">>>> Rp.")
+		fmt.Scan(&data[n-1].harga)
+		for data[n-1].harga == 0 {
+			fmt.Println("Harga produk kosong! Mohon masukkan harga produk: ")
+			fmt.Print(">>>> Rp.")
+			fmt.Scan(&data[n-1].harga)
+		}
+		fmt.Println("---------------------------")
+		fmt.Println("Stok Produk:")
+		fmt.Print(">>>> ")
+		fmt.Scan(&data[n-1].stok)
+		for data[n-1].stok == -1 {
+			fmt.Println("Stok produk kosong! Mohon masukkan stok produk: ")
+			fmt.Print(">>>> ")
+			fmt.Scan(&data[n-1].stok)
+		}
+		fmt.Println("---------------------------")
+		fmt.Println("Data Ke-", n, "Berhasil Diinput")
 	}
 }
 
@@ -182,13 +260,13 @@ func menuStart() {
 }
 
 func menuProcess() {
-	fmt.Println("-----------------------------------------------")
-	fmt.Println("               M E N U   F I T U R             ")
-	fmt.Println("-----------------------------------------------")
+	fmt.Println("|-----------------------------------------------|")
+	fmt.Println("|               M E N U   F I T U R             |")
+	fmt.Println("|-----------------------------------------------|")
 	fmt.Println("1. Konfigurasi Data Produk")
 	fmt.Println("2. Pencatatan Transaksi")
 	fmt.Println("3. Exit Program")
-	fmt.Println("-----------------------------------------------")
+	fmt.Println("|-----------------------------------------------|")
 }
 
 func menuEnd() {
@@ -197,6 +275,27 @@ func menuEnd() {
 	fmt.Println("                Program Selesai                ")
 	fmt.Println("-----------------------------------------------")
 	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+}
+
+func menuOptionsTampilSemuaProduk() {
+	menuHeaderKonfigurasiDataProduk()
+	fmt.Println("1. Lihat data")
+	fmt.Println("2. Edit Data")
+	fmt.Println("3. Hapus Data")
+	fmt.Println("4. Cari Data")
+	fmt.Println("5. Urutkan Data")
+}
+
+func menuHeaderKonfigurasiDataProduk() {
+	fmt.Println("|---------------------------------|")
+	fmt.Println("| M E N U - P R O D U K - D A T A |")
+	fmt.Println("|---------------------------------|")
+}
+
+func menuOptionsKonfigurasiDataProduk() {
+	fmt.Println("1. Tambah Data")
+	fmt.Println("2. Tampilkan Semua Data")
+	fmt.Println("3. Kembali ke Menu Utama")
 }
 
 // ---------------------------------------------------------------
